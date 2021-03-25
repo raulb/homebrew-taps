81551cce4f1f96a68cda169c84f1b61b7eabbf24
Raúl Barroso <raul@meroxa.io>
2021-03-25 18:21:50 +0100

    Update meroxa.rb

--- Formula/meroxa.rb
+++ Formula/meroxa.rb
@@ -2,7 +2,7 @@ class Meroxa < Formula
   desc "The Meroxa CLI"
   homepage "https://meroxa.io/"
   url "https://github.com/meroxa/cli/releases/download/v0.2.3/meroxa_0.2.3_Darwin_x86_64.tar.gz"
-  sha256 "54f24817bf14e1cde5675c8cbed38a1b9786c8e0"
+  sha256 "678c9b98c05147eda95bc5fde5ff60d1b3f1c75fabf4102816240daaf395b657"
   license "Apache-2.0"
   
   def install
