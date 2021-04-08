e8f7c80b11119ceef344250eee13e0068f40c9b8
goreleaserbot <goreleaser@carlosbecker.com>
2021-04-08 15:37:58 +0200

    Brew formula update for meroxa version v0.3.4

--- Formula/meroxa.rb
+++ Formula/meroxa.rb
@@ -11,27 +11,29 @@ class Meroxa < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/raulb/cli/releases/download/v0.3.4/meroxa_0.3.4_Darwin_x86_64.tar.gz"
-    sha256 "a7e22bff2bd88b377757de9fc3a3c205c9dc9199a35524bd9958ef1f1d1a09d1"
+    sha256 "c719885ae5ed57f5b627f9b2c3cd43de6991d02438485e24dd9537d0ffc0ce56"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/raulb/cli/releases/download/v0.3.4/meroxa_0.3.4_Darwin_arm64.tar.gz"
-    sha256 "a2ddc2348a96c95f39875fe5a8c9c22ca5acb16b23261710428ea6a101fb30f4"
+    sha256 "1870f0e70f2702315a638fea2d63edd2d384f33ef4a9788cd00732ea011ecde1"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/raulb/cli/releases/download/v0.3.4/meroxa_0.3.4_Linux_x86_64.tar.gz"
-    sha256 "3d33035c8985d746612694212fc09cf08eed98b35b93da0dc11f40d852d75013"
+    sha256 "f201be347bf4026b8864b1fda6e697b826ccfbdb5762479fa2bacb9ad9c3d7f9"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/raulb/cli/releases/download/v0.3.4/meroxa_0.3.4_Linux_arm64.tar.gz"
-    sha256 "9f7404efb6fe3792455640d32db2afdecd0bba59f6585ea5db40202a059b677e"
+    sha256 "bf0c1922925847a60002dd60af2cfb381bc640f701a9ea39c57aabc3b070a675"
   end
 
   head "https://github.com/raulb/cli.git"
 
-  depends_on "go"
-
   def install
     bin.install "meroxa"
+    bash_completion.install "etc/completion/meroxa.completion.sh"
+    zsh_completion.install "etc/completion/meroxa.completion.zsh" => "meroxa"
+    fish_completion.install "etc/completion/meroxa.completion.fish"
+    man.install "etc/man/man1"
   end
 
   test do
